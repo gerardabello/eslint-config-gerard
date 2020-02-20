adcd606a8ba750677a53bf91fd6d795f9b1fc7d9
Gerard Abelló Serras <gerardabello@gmail.com>
2020-02-20 09:21:05 +0100

    remove styling rules

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 module.exports = {
   parser: 'babel-eslint',
-  extends: [
-    'standard',
-    'standard-jsx',
-    'plugin:react/recommended',
-    'prettier/react',
-    'prettier-standard'
-  ],
-  plugins: ['filenames', 'react', 'prettier', 'react-perf', 'react-hooks'],
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  plugins: ['filenames', 'react', 'import', 'react-hooks'],
   settings: {
     react: {
       version: '16'
@@ -31,18 +25,30 @@ module.exports = {
   globals: { fixture: true, FS: true },
   rules: {
     'filenames/match-regex': ['warn', '^_?[a-z0-9-.]+$'],
-    'prettier/prettier': 'error',
-    'react-perf/jsx-no-new-object-as-prop': 0,
-    'react-perf/jsx-no-new-array-as-prop': 0,
-    'react-perf/jsx-no-new-function-as-prop': 0,
-    'react-hooks/rules-of-hooks': 'error',
-    "react/no-unsafe": 'warn',
+
+    'import/no-unresolved': 'error',
+    'import/named': 'error',
+    'import/export': 'error',
+    'import/default': 'error',
+    'import/no-unused-modules': 'warn',
+
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+
     'react/display-name': 'warn',
     'react/prop-types': 'warn',
-    'react/jsx-no-bind': 0,
     'react/jsx-handler-names': 'warn',
-    'curly': 'error',
-    'no-shadow': 'warn',
+    'react/boolean-prop-naming': 'warn',
+    'react/button-has-type': 'error',
+    'react/default-props-match-prop-types': 'warn',
+    'react/jsx-boolean-value': 'error',
+    'react/jsx-no-duplicate-props': 'error',
+    'react/jsx-key': 'error',
+
+    eqeqeq: 'error',
+    'no-return-await': 'error',
+    curly: 'error',
+    'no-shadow': 'error',
     'no-else-return': 'warn',
     'no-nested-ternary': 'warn',
     'no-unneeded-ternary': 'warn',
@@ -56,3 +62,4 @@ module.exports = {
     ]
   }
 }
+

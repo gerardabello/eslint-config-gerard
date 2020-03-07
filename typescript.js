ca330e07e49ca4b6e80cc6958acc517288bd4c11
Gerard Abelló Serras <gerardabello@gmail.com>
2020-03-07 23:53:07 +0100

    add typescript

---
+++ typescript.js
@@ -0,0 +1,14 @@
+const jsConfig = require('./index.js')
+
+module.exports = {
+  ...jsConfig,
+  root: true,
+  parser: "@typescript-eslint/parser",
+  extends: [
+    ...jsConfig.extends
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  plugins: [...jsConfig.plugins, '@typescript-eslint']
+}
+
